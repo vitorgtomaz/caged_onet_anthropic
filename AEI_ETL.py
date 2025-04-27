851fbbcbe007457e7e9fc95e4e63ee320d5c5fa1
vitorgtomaz <106792708+vitorgtomaz@users.noreply.github.com>
2025-04-27 20:35:02 +0400

    Final analysis

--- AEI_ETL.py
+++ AEI_ETL.py
@@ -16,11 +16,11 @@ def merge_onet_soc_data() -> pd.DataFrame:
     """
 
     # Read and process O*NET data
-    onet_df = pd.read_csv("onet_task_statements.csv")
+    onet_df = pd.read_csv("data/input/aei_data/onet_task_statements.csv")
     onet_df["soc_major_group"] = onet_df["O*NET-SOC Code"].str[:2]
 
     # Read and process SOC data
-    soc_df = pd.read_csv("SOC_Structure.csv")
+    soc_df = pd.read_csv("data/input/aei_data/SOC_Structure.csv")
     soc_df = soc_df.dropna(subset=["Major Group"])
     soc_df["soc_major_group"] = soc_df["Major Group"].str[:2]
 
@@ -34,22 +34,19 @@ def merge_onet_soc_data() -> pd.DataFrame:
     return merged_df
 
 
-task_occupations_df = merge_onet_soc_data()
-task_occupations_df["Title"].nunique()
+onet_df = merge_onet_soc_data()
 
 # Update cluster mappings to include data from the merged_df
-task_occupations_df["task_normalized"] = (
-    task_occupations_df["Task"].str.lower().str.strip()
-)
+onet_df["task_normalized"] = onet_df["Task"].str.lower().str.strip()
 # Some tasks are included multiple times, so we need to count the number of occurrences per task
-task_occupations_df["n_occurrences"] = task_occupations_df.groupby("task_normalized")[
-    "Title"
-].transform("nunique")
+onet_df["n_occurrences"] = onet_df.groupby("task_normalized")["Title"].transform(
+    "nunique"
+)
 
-task_mappings_df = pd.read_csv("task_pct_v2.csv")
+task_mappings_df = pd.read_csv("data/input/aei_data/task_pct_v2.csv")
 
 grouped_with_occupations = task_mappings_df.merge(
-    task_occupations_df, left_on="task_name", right_on="task_normalized", how="left"
+    onet_df, left_on="task_name", right_on="task_normalized", how="left"
 )
 
 grouped_with_occupations["pct_occ_scaled"] = (
@@ -62,10 +59,67 @@ grouped_with_occupations["pct_occ_scaled"] = (
 grouped_with_occupations["pct_occ_scaled"].sum()
 
 automation_vs_augmentation_by_task_df = pd.read_csv(
-    "automation_vs_augmentation_by_task.csv"
+    "data/input/aei_data/automation_vs_augmentation_by_task.csv"
 )
 
 automation_vs_augmentation_with_occupations = grouped_with_occupations.merge(
     automation_vs_augmentation_by_task_df, on="task_name", how="left"
 )
 assert len(automation_vs_augmentation_with_occupations) == len(grouped_with_occupations)
+
+
+import pandas as pd
+import numpy as np
+
+automation_vs_augmentation_by_task_df = pd.read_csv(
+    "data/input/aei_data/automation_vs_augmentation_by_task.csv"
+)
+
+task_pct = pd.read_csv("data/input/aei_data/task_pct_v2.csv")
+
+onet_df = pd.read_csv("data/input/aei_data/onet_task_statements.csv")
+onet_df["soc_minor_group"] = onet_df["O*NET-SOC Code"].str[:4]
+onet_df["task_normalized"] = onet_df["Task"].str.lower().str.strip()
+onet_df = onet_df[["soc_minor_group", "task_normalized"]]
+onet_df.rename({"task_normalized": "task_name"}, axis=1, inplace=True)
+onet_df.drop_duplicates(inplace=True)
+
+df = task_pct.merge(automation_vs_augmentation_by_task_df, on="task_name", how="left")
+df.fillna(0, inplace=True)
+df["aug"] = (df["learning"] + df["validation"]) * df["pct"]
+df["aut"] = (df["feedback_loop"] + df["directive"] + df["task_iteration"]) * df["pct"]
+
+df.drop(
+    columns=["learning", "validation", "feedback_loop", "directive", "task_iteration"],
+    inplace=True,
+)
+
+df = df.merge(onet_df, on="task_name", how="left")
+
+# Sum of pct is smaller than 100% because ~1.7% belong to rows with no occupation
+occ_aut_aug_lvl = (
+    df.groupby(["soc_minor_group"])[["pct", "aug", "aut"]].sum().reset_index()
+)
+occ_aut_aug_lvl["aug_aut_ratio"] = occ_aut_aug_lvl["aug"] / occ_aut_aug_lvl["aut"]
+
+occ_aut_aug_lvl.to_parquet("data/output/occ_aut_aug_lvl.parquet")
+
+# Classifying jobs
+## 1. Highest usage jobs
+top_10_jobs = occ_aut_aug_lvl.sort_values("pct", ascending=False).head(10)
+top_10_jobs["class"] = "Top 10 pct"
+
+## 2. Lowest usage jobs
+bottom_10_jobs = occ_aut_aug_lvl.sort_values("pct", ascending=True).head(10)
+bottom_10_jobs["class"] = "Bottom 10 pct"
+
+## 3. jobs with highest aut
+top_10_aut_jobs = occ_aut_aug_lvl.sort_values("aut", ascending=False).head(10)
+top_10_aut_jobs["class"] = "Top 10 aut"
+
+## 4. jobs with highest aug
+top_10_aug_jobs = occ_aut_aug_lvl.sort_values("aug", ascending=False).head(10)
+top_10_aug_jobs["class"] = "Top 10 aug"
+
+df = pd.concat([top_10_jobs, bottom_10_jobs, top_10_aut_jobs, top_10_aug_jobs])
+df.to_parquet("data/output/occ_aut_aug_lvl_classified.parquet")
--- CAGED_ETL.py
+++ CAGED_ETL.py
@@ -28,12 +28,13 @@ with open(query_file, "r") as f:
         query = query + f"\nLIMIT {LIMIT};"
 
 
-df = bd.read_sql(query=query, billing_project_id=billing_id)
-df.to_parquet("data/input/caged_national_UNTREATED.parquet")
+# df = bd.read_sql(query=query, billing_project_id=billing_id)
+# df.to_parquet("data/input/caged_national_UNTREATED.parquet")
 
 df = pd.read_parquet("data/input/caged_national_UNTREATED.parquet")
+df = df[~df["cbo_2002_descricao_subgrupo_principal"].isna()]
 
-CW_CAGED = json.load(open("data/config/caged_crosswalks.json", encoding="utf-8"))
+# CW_CAGED = json.load(open("data/config/caged_crosswalks.json", encoding="utf-8"))
 
 # Data adjustments
 cols_to_keep = [
@@ -41,21 +42,19 @@ cols_to_keep = [
     "mes",
     "cbo_2002_descricao_subgrupo_principal",
     "cbo_2002_descricao_grande_grupo",
-    "grau_instrucao",
-    "tipo_estabelecimento",
-    "tipo_movimentacao",
 ]
+
 df = (
-    df.groupby(cols_to_keep)["saldo_movimentacao"]
+    df.groupby(cols_to_keep, dropna=False)["saldo_movimentacao"]
     .sum()
     .reset_index()
     .apply(lambda col: col.str.strip() if col.dtype == "object" else col)
 )
 
-df["employed_by"] = df["tipo_estabelecimento"].map(CW_CAGED["tipo_estabelecimento"])
-df[["movement_type", "movement_subtype"]] = (
-    df["tipo_movimentacao"].map(CW_CAGED["tipo_movimentacao"]).apply(pd.Series)
-)
+# df["employed_by"] = df["tipo_estabelecimento"].map(CW_CAGED["tipo_estabelecimento"])
+# df[["movement_type", "movement_subtype"]] = (
+#    df["tipo_movimentacao"].map(CW_CAGED["tipo_movimentacao"]).apply(pd.Series)
+# )
 
 df = df.rename(
     columns={
@@ -64,15 +63,15 @@ df = df.rename(
         "mes": "month",
         "cbo_2002_descricao_subgrupo_principal": "cbo_subgroup",
         "cbo_2002_descricao_grande_grupo": "cbo_group",
-        "grau_instrucao": "education_level",
+        #        "grau_instrucao": "education_level",
     }
 )
 
 # remove columns 'tipo_movimentacao' and 'movement_subtype
-df = df.drop(columns=["tipo_movimentacao", "tipo_estabelecimento"])
+# df = df.drop(columns=["tipo_movimentacao", "tipo_estabelecimento"])
 
 df["cbo_subgroup"] = df["cbo_subgroup"].apply(strip_accents)
 df["cbo_group"] = df["cbo_group"].apply(strip_accents)
-df["education_level"] = df["education_level"].apply(strip_accents)
+# df["education_level"] = df["education_level"].apply(strip_accents)
 
 df.to_parquet("data/input/caged_national.parquet")
